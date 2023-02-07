b0f84d6846d25ab8bcaad107cdfa7abbdb3f1d2f
YohanesGetinet1 <johnget496@gmail.com>
2023-02-07 19:48:57 +0530

    Task 1 async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-'''async_comprehension'''
-
-
-import asyncio
-import random
+"""async_comprehension"""
 
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension()-> list[float]:
-    '''async_comprehension coroutine'''
+    """async_comprehension coroutine"""
     return [x async for x in async_generator()]

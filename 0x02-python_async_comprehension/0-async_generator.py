d92dd2d71b44cda63554495c31ce1828cd50f3c8
YohanesGetinet1 <johnget496@gmail.com>
2023-02-07 20:34:09 +0530

    Fixed bugs

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""coroutine async_generator"""
+"""
+coroutine async_generator
+Write coroutine async_generator that takes two arguments
+loops 10x, wait 1sec each time asynchronously
+yield random number between 0-10 using random module
+"""
 import asyncio
 import random
 from typing import Generator
@@ -8,4 +13,4 @@ async def async_generator() -> Generator [float, None, None]:
     """async_generator"""
     for x in range(0, 10,1):
         await asyncio.sleep(1)
-        yield random.randint(0,10)
+        yield random.uniform(0,10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """async_comprehension"""
+from typing import List
+result = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension()-> list[float]:
+async def async_comprehension()-> result:
     """async_comprehension coroutine"""
-    return [x async for x in async_generator()]
+    stop = [x async for x in async_generator()]
+    return stop

2fdd292647742c3a2b7b5c8e426f4a4b71be6617
YohanesGetinet1 <johnget496@gmail.com>
2023-02-15 16:07:41 +0530

    Task 1 Coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""execute multiple coroutines at the same time with async"""
+"""
+Execute multiple coroutines same time with async
+Write async coroutine
+takes argument wait_n and max_delay
+"""
 import asyncio
+from typing import List
+import random
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay : int = 10) -> list[float]:
-    """return the list of all the delays (float values)"""
-    delay_list: list=[]
-    for i in range(n):
-        delay = asyncio.create_task(wait_random(max_delay))
-        delay_list.append(delay)
-    result_list: list=[]
-    for i in asyncio.as_completed(delay_list):
-        result: float = await i
-        result_list.append(result)
-    return result_list 
-# print(asyncio.run(wait_n(5, 5)))
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine
+    wait_n
+    """
+    ls = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    end = [await task for task in asyncio.as_completed(ls)]
+    return end
